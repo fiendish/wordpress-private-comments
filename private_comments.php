1749d848df87f30774609804dad8ee45a24a16e6
Avi Kelman <patcherton.fixesthings@gmail.com>
2014-09-22 17:21:19 -0400

    update license

--- private_comments.php
+++ private_comments.php
@@ -6,7 +6,25 @@ Description: Comments are only visible to the allowed group.
 Author: Avi Kelman
 Version: 1.0.0
 Author URI: https://www.github.com/fiendish
-*/ 
+License: GPLv3
+*/
+
+/*
+    Copyright (C) 2014  Avi Kelman ( email: patcherton.fixesthings@gmail.com )
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 function plugin_private_allowed_group()
 {
